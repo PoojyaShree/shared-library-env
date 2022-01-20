00b9458976c3797f69ae27b5ccc97cd7a1e8030e
PoojyaShree <87475087+PoojyaShree@users.noreply.github.com>
2022-01-20 18:06:55 +0530

    Update myclass.groovy

--- src/myclass.groovy
+++ src/myclass.groovy
@@ -1,3 +1,8 @@
-MyClass(def script) {
+/*MyClass(def script) {
   throw new Exception("FOO=${script.env.FOO}")
+}*/
+class myclass implements Serializable {
+  void mymethod(steps) {
+    String myEnvVar = steps.sh(returnStdout: true, script: "env | grep 'myVar' | cut -f 2- -d '='")
+  }
 }
